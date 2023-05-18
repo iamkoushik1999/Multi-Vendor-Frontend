c91cfc32018a220ab935e9fb3c82c34c59e37771
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-18 23:21:24 +0530

    User Activation (LoadUser) Redux implemented

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage, SignUpPage, ActivationPage } from "./Routes.js";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
-import { server } from "./server";
+import Store from "./redux/store";
+import { loadUser } from "./redux/actions/userAction";
 
 const App = () => {
   useEffect(() => {
-    axios
-      .get(`${server}/user/getUser`, { withCredentials: true })
-      .then((res) => {
-        // console.log(res.data);
-        toast.success(res.data.message);
-      })
-      .catch((err) => {
-        // console.log(err);
-        toast.error(err.response.data.message);
-      });
+    Store.dispatch(loadUser());
   }, []);
   return (
     <BrowserRouter>
