f461647dc2768a24d647bbbc92b280d9fef52cdd
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-18 23:15:20 +0530

    User Activation(getUser)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage, SignUpPage, ActivationPage } from "./Routes.js";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { server } from "./server";
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get(`${server}/user/getUser`, { withCredentials: true })
+      .then((res) => {
+        // console.log(res.data);
+        toast.success(res.data.message);
+      })
+      .catch((err) => {
+        // console.log(err);
+        toast.error(err.response.data.message);
+      });
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
