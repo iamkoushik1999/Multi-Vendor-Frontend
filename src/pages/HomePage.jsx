ab000c3a234c771e4ee68d1a9c582dba57bf7def
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-22 22:16:23 +0530

    Featured Products Frontend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
+import FeaturedProducts from "../components/Route/FeaturedProducts/FeaturedProducts";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Hero />
       <Categories />
       <BestDeals />
+      <FeaturedProducts />
     </div>
   );
 };
