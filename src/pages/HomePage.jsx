cabc14e8d65f65d27690c3f3624bcb9d03f07312
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-22 22:18:34 +0530

    Events Card Frontend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProducts from "../components/Route/FeaturedProducts/FeaturedProducts";
+import Events from "../components/Events/Events";
 
 const HomePage = () => {
   return (
@@ -12,6 +13,7 @@ const HomePage = () => {
       <Hero />
       <Categories />
       <BestDeals />
+      <Events />
       <FeaturedProducts />
     </div>
   );
