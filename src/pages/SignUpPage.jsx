99980ffdef89fcfae3fa67415a05065041a2eb69
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-23 22:01:25 +0530

    authenticating LoginPage & SignupPage

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Signup from "../components/Signup/Signup.jsx";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <div>
       <Signup />
