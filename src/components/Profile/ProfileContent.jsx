cbe31654f4e8e367663ef446d8a49df5b59844e4
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-24 22:12:43 +0530

    Profile -> ProfileContent

--- src/components/Profile/ProfileContent.jsx
+++ src/components/Profile/ProfileContent.jsx
@@ -1,7 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import { backend_url } from "../../server";
+import { useSelector } from "react-redux";
+import { AiOutlineCamera } from "react-icons/ai";
+import styles from "../../styles/styles";
 
-const ProfileContent = () => {
-  return <div>ProfileContent</div>;
+const ProfileContent = ({ active }) => {
+  const { user } = useSelector((state) => state.user);
+  const [name, setName] = useState(user && user.name);
+  const [email, setEmail] = useState(user && user.email);
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [zipCode, setZipCode] = useState();
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="w-full">
+      {/* Profile Page */}
+      {active === 1 && (
+        <>
+          <div className="flex justify-center w-full">
+            <div className="relative">
+              <img
+                src={`${backend_url}${user?.avatar}`}
+                alt="my profile"
+                className="w-[150px] h-[150px] rounded-full object-cover border-[3px] border-[#3ad132] "
+              />
+              <div className="w-[30px] h-[30px] bg-[#e3e9ee] rounded-full flex items-center justify-center cursor-pointer absolute bottom-[5px] right-[5px]">
+                <AiOutlineCamera />
+              </div>
+            </div>
+          </div>
+          <br /> <br />
+          <div className="w-full px-5">
+            <form onSubmit={handleSubmit} aria-required={true}>
+              <div className="w-full flex pb-3">
+                <div className="w-[50%]">
+                  <label className="block pb-2">Full Name</label>
+                  <input
+                    type="text"
+                    className={`${styles.input} !w-[95%] `}
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="w-[50%]">
+                  <label className="block pb-2">Email Address</label>
+                  <input
+                    type="email"
+                    className={`${styles.input} !w-[95%] `}
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="w-full flex pb-3">
+                <div className="w-[50%]">
+                  <label className="block pb-2">Phone Number</label>
+                  <input
+                    type="number"
+                    className={`${styles.input} !w-[95%] `}
+                    required
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+                <div className="w-[50%]">
+                  <label className="block pb-2">Zip Code</label>
+                  <input
+                    type="number"
+                    className={`${styles.input} !w-[95%] `}
+                    required
+                    value={zipCode}
+                    onChange={(e) => setZipCode(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="w-full flex pb-3">
+                <div className="w-[50%]">
+                  <label className="block pb-2">Address 1</label>
+                  <input
+                    type="text"
+                    className={`${styles.input} !w-[95%] `}
+                    required
+                    value={address1}
+                    onChange={(e) => setAddress1(e.target.value)}
+                  />
+                </div>
+                <div className="w-[50%]">
+                  <label className="block pb-2">Address 2</label>
+                  <input
+                    type="text"
+                    className={`${styles.input} !w-[95%] `}
+                    required
+                    value={address2}
+                    onChange={(e) => setAddress2(e.target.value)}
+                  />
+                </div>
+              </div>
+              <input
+                className={`w-[250px] h-[40px] border border-[#3a24db] text-center text--[#3a24db] rounded-[3px] mt-8 cursor-pointer`}
+                type="submit"
+                value="Update"
+                required
+              />
+            </form>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
-
 export default ProfileContent;
