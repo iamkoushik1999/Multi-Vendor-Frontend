d882b51bd130179079c4b04964ed06bd1c2e4d29
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-24 22:29:54 +0530

    User Logout & ProtectedRoute Profile

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ import "react-toastify/dist/ReactToastify.css";
 import Store from "./redux/store";
 import { loadUser } from "./redux/actions/userAction";
 import { useSelector } from "react-redux";
+import ProtectedRoute from "./ProtectedRoute";
 
 const App = () => {
-  const { loading } = useSelector((state) => state.user);
+  const { loading, isAuthenticated } = useSelector((state) => state.user);
   useEffect(() => {
     Store.dispatch(loadUser());
   }, []);
@@ -42,7 +43,14 @@ const App = () => {
             <Route path="/best-selling" element={<BestSellingPage />} />
             <Route path="/events" element={<EventsPage />} />
             <Route path="/faq" element={<FAQPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <ToastContainer
             position="top-center"
