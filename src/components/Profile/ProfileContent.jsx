b14bb78e252cd4e8adcc5f1fb45f7757c23b6122
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-24 22:16:46 +0530

    Profile -> ProfileContent -> Orders

--- src/components/Profile/ProfileContent.jsx
+++ src/components/Profile/ProfileContent.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { backend_url } from "../../server";
 import { useSelector } from "react-redux";
-import { AiOutlineCamera } from "react-icons/ai";
+import { AiOutlineArrowRight, AiOutlineCamera } from "react-icons/ai";
 import styles from "../../styles/styles";
+import { Link } from "react-router-dom";
+import { DataGrid } from "@material-ui/data-grid";
+import { Button } from "@material-ui/core";
 
 const ProfileContent = ({ active }) => {
   const { user } = useSelector((state) => state.user);
@@ -112,7 +115,104 @@ const ProfileContent = ({ active }) => {
           </div>
         </>
       )}
+      {/* Order page */}
+      {active === 2 && (
+        <div>
+          <AllOrders />
+        </div>
+      )}
     </div>
   );
 };
+
+const AllOrders = () => {
+  const orders = [
+    {
+      _id: "dljfbndkbvbjdv",
+      orderItems: [
+        {
+          name: "Iphone",
+        },
+      ],
+      totalPrice: 120,
+      status: "Processing",
+    },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.orderItems.length,
+        total: "US$ " + item.totalPrice,
+        status: item.status,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+        autoHeight
+      />
+    </div>
+  );
+};
+
 export default ProfileContent;
