3bebb3d3070ed0ac63bfe5b252c09e80028af1fc
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-24 22:22:15 +0530

    Profile -> ProfileContent -> TrackOrder

--- src/components/Profile/ProfileContent.jsx
+++ src/components/Profile/ProfileContent.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { backend_url } from "../../server";
 import { useSelector } from "react-redux";
 import { AiOutlineArrowRight, AiOutlineCamera } from "react-icons/ai";
+import { MdTrackChanges } from "react-icons/md";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { DataGrid } from "@material-ui/data-grid";
@@ -125,6 +126,12 @@ const ProfileContent = ({ active }) => {
       {active === 3 && (
         <div>
           <AllRefundOrders />
+        </div>
+      )}
+      {/* Track Order page */}
+      {active === 5 && (
+        <div>
+          <TrackOrder />
         </div>
       )}
     </div>
@@ -278,6 +285,96 @@ const AllRefundOrders = () => {
             <Link to={`/user/order/${params.id}`}>
               <Button>
                 <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.orderItems.length,
+        total: "US$ " + item.totalPrice,
+        status: item.status,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+        autoHeight
+      />
+    </div>
+  );
+};
+
+const TrackOrder = () => {
+  const orders = [
+    {
+      _id: "dljfbndkbvbjdv",
+      orderItems: [
+        {
+          name: "Iphone",
+        },
+      ],
+      totalPrice: 120,
+      status: "Processing",
+    },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/track/order/${params.id}`}>
+              <Button>
+                <MdTrackChanges size={20} />
               </Button>
             </Link>
           </>
